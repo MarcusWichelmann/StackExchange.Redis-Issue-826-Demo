0afb5922c12dfc0bd8ef348c003faf987a12cbc9
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2018-05-28 19:42:28 +0200

    Projektdateien hinzufügen.

---
+++ RedisAspPerformanceTest/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisAspPerformanceTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        private readonly ConnectionMultiplexer _client;
+
+        public TestController(ConnectionMultiplexer client)
+        {
+            _client = client;
+        }
+
+        // Test with:
+        // wrk -t8 -c400 -d60s --latency http://localhost:PORT/api/test/method
+        // using "wrk" from https://github.com/wg/wrk
+
+        [HttpGet("method")]
+        public IActionResult Get()
+        {
+            IDatabase database = _client.GetDatabase();
+            string key = $"testkey:{Guid.NewGuid()}";
+
+            Console.WriteLine("-- StringSet");
+            database.StringSet(key, "test");
+            Console.WriteLine("-- -- Return");
+
+            return Ok();
+        }
+    }
+}
---
+++ RedisAspPerformanceTest/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+
+namespace RedisAspPerformanceTest
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var client = ConnectionMultiplexer.Connect(new ConfigurationOptions {
+                EndPoints = { { "10.200.1.100", 6379 } },
+                AbortOnConnectFail = false,
+                ConnectTimeout = 15000
+            }, Console.Out);
+
+            client.PreserveAsyncOrder = false;
+
+            services.AddSingleton(client);
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
